9e7ce6e2ce146190bbdc7132284af7f27c53bdde
BrBrov <epoc@list.ru>
2023-05-10 16:09:30 +0300

    feat: add classes for conversion

--- utils/convert.js
+++ utils/convert.js
@@ -1,3 +1,5 @@
+//  all data was getting from https://www.aviasales.ru/
+
 const ru = require('./ru.json');
 const en = require('./en.json');
 
@@ -11,5 +13,36 @@ class City {
   GMT = ''; //?
   location = ''; //country_name
   isOpen = true;
-  priceTarif = ''; //?
+  priceTarif = ''; //?  
+  tickets = [];
+}
+
+class Ticket{
+  date = ''; // ISO-8601 date and time to flight
+  placeCount = 0; //count must be from 0 to 100
+  direction = ''; //city destination
+
+  #randPlaces() {
+    return Math.ceil(Math.random() * 100);
+  }
+}
+
+class ConsructorCity extends City {
+  constructor (en, ru, id) {
+    this.#create(en, ru);
+  }
+
+  #create(en, ru, id) {
+    this.id = id;
+    this.nameEn = en.name;
+    this.nameRu = ru.name;
+    this.airportEn = en.main_airport_name;
+    this.airportRu = ru.main_airport_name;
+    this.abbreviation = en.code;
+    this.location = en.country_name;
+  }
+
+  #createTickets(count) {
+    
+  }
 }
